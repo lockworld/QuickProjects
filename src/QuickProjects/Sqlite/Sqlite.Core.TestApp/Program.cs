b96400ad8f38b7cb486aed2c47966821b16f6759
Douglas Lockwood <dlockwood@allosource.org>
2023-01-27 16:51:07 -0700

    Committing to remove from local repository

---
+++ src/QuickProjects/Sqlite/Sqlite.Core.TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace Sqlite.Core.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var connection = new SqliteConnection("Data Source=app.db"))
+            {
+                connection.Open();
+
+                connection.Close();
+            }
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
